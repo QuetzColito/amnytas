ef85a42a38d297156faa4a2943763aa8174fc6e3
Stefan Lahne <stefan.lahne@gmx.de>
2024-05-01 00:20:48 +0200

    fix mabon hostname

--- system/machines/mabon/machine.nix
+++ system/machines/mabon/machine.nix
@@ -3,7 +3,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "arthezia"; # Define your hostname.
+  networking.hostName = "mabon"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Nvidia
