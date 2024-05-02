47103a975a5d3c1115f7f1962a7a862d2ab2371c
Stefan Lahne <stefan.lahne@gmx.de>
2024-05-02 18:12:53 +0200

    add vscode extensions

--- programs/programs.nix
+++ programs/programs.nix
@@ -11,6 +11,16 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions; [
+      redhat.java
+      haskell.haskell
+      vscodevim.vim
+      pkief.material-icon-theme
+      justusadam.language-haskell
+      jnoortheen.nix-ide
+      johnpapa.vscode-peacock
+      enkia.tokyo-night
+    ];
   };
 
   services.dunst = {
