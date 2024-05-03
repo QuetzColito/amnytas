ceb045b1db8d6aa9255748ba1bf4e52fc6398242
Stefan Lahne <stefan.lahne@gmx.de>
2024-05-03 11:22:25 +0200

    add wpa for mabon

--- home-mabon.nix
+++ home-mabon.nix
@@ -25,6 +25,11 @@
 
   programs.waybar.settings.mainbar.output = ["eDP-1"];
 
+  home.packages = with pkgs; [
+    wpa_supplicant
+    wpa_supplicant_gui
+  ];
+
   wayland.windowManager.hyprland.settings = {
     workspace = [
       "1, monitor:eDP-1"
