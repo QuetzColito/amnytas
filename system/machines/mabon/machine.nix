f08a8e74fbd41af36d42de4c19bd44afc511950b
Stefan Lahne <stefan.lahne@gmx.de>
2024-05-05 23:41:43 +0200

    try touchpad stuff

--- system/machines/mabon/machine.nix
+++ system/machines/mabon/machine.nix
@@ -7,7 +7,8 @@
   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.wireless.userControlled.enable = true;
 
-
+  services.xserver.libinput.enable = true;
+  services.xserver.libinput.touchpad.tapping = true;
 
   # Nvidia
   hardware.opengl = {
