9fd915fd302931753b483206d440a248dc5efcda
Stefan Lahne <stefan.lahne@gmx.de>
2024-05-16 21:06:19 +0200

    try sr w/o checksum

--- system/general.nix
+++ system/general.nix
@@ -1,9 +1,6 @@
 {pkgs, lib, config, ...}:
 let
- aagl-gtk-on-nix = import (builtins.fetchTarball {
-   url = "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz";
-   sha256 = sha256:1jd27cswy4y8yars86vj0mhjlhym8mgrzhlwlaks95hkjx6zm48z;
-   });
+ aagl-gtk-on-nix = import (builtins.fetchTarball "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz");
 in
 {
   # Enable networking
