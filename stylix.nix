27658d3903564a198fd84ea0e3ebcb028bb40f17
Stefan Lahne <stefan.lahne@gmx.de>
2024-05-20 20:27:58 +0200

    remove fonts from stylix

--- stylix.nix
+++ stylix.nix
@@ -3,27 +3,6 @@
   ...
 } : {
   stylix = {
-    fonts = {
-      serif = {
-        package = (pkgs.nerdfonts.override {fonts = ["Iosevka"];});
-        name = "Iosevka";
-      };
-
-      sansSerif = {
-        package = (pkgs.nerdfonts.override {fonts = ["Iosevka"];});
-        name = "Iosevka";
-      };
-
-      monospace = {
-        package = (pkgs.nerdfonts.override {fonts = ["Iosevka"];});
-        name = "Iosevka Term";
-      };
-
-      emoji = {
-        package = pkgs.noto-fonts-emoji;
-        name = "Noto Color Emoji";
-      };
-    };
 
     base16Scheme = {
       base01= "1A1B26";
