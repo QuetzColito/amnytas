567592c809248727232768bafc49fb488bcda6e5
Stefan Lahne <stefan.lahne@gmx.de>
2024-05-24 17:28:49 +0200

    hyprland keybinds

--- home/rice/hyprland/binds.nix
+++ home/rice/hyprland/binds.nix
@@ -21,7 +21,12 @@ in {
   wayland.windowManager.hyprland.settings = {
     bind =
       [
-        ''${mod},RETURN,exec,alacritty''
+        # App Shortcuts
+        "${mod},RETURN,exec,alacritty"
+        "${mod},N,exec,thunar"
+        "${mod},S,exec,codium"
+        "${mod},Y,exec,for i in ~/MyGames/YTM/*.AppImage ; do appimage-run $i; done" # EXTERNAL DEPENDENCY
+        "${mod},D,exec,flatpak run dev.vencord.Vesktop" # EXTERNAL DEPENDENCY
 
         "${mod},SPACE,exec,tofi-drun --drun-launch=true"
         "${mod},Q,killactive"
@@ -45,16 +50,21 @@ in {
         # workspace controls
         "${modshift},right,movetoworkspace,+1" # move focused window to the next ws
         "${modshift},left,movetoworkspace,-1" # move focused window to the previous ws
-        "${mod},right,workspace,+1" # move focused window to the next ws
-        "${mod},left,workspace,-1" # move focused window to the previous ws
+        "${mod},right,workspace,+1" # move focus to the next ws
+        "${mod},left,workspace,-1" # move focus to the previous ws
         "${mod},mouse_up,workspace,e+1" # move to the next ws
         "${mod},mouse_down,workspace,e-1" # move to the previous ws
+
+        # new monitor key
+        "${modshift},N,moveworkspacetomonitor,3 1"
+        "${modshift},N,moveworkspacetomonitor,4 1"
+        "${modshift},N,moveworkspacetomonitor,5 1"
+        "${modshift},N,moveworkspacetomonitor,6 1"
+
 
         "CTRL ${modshift},S,exec, pauseshot"
         "${modshift},S,exec, grim -g \"$(slurp)\" - | wl-copy"
         "${modshift},O,exec,wl-ocr"
-
-        "${mod},Period,exec, tofi-emoji"
 
         "${modshift},L,exec,hyprlock"
       ]
