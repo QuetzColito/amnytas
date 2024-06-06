61aba7a4ea0905ae43dfb72b5876997ec849ca0b
Stefan Lahne <stefan.lahne@gmx.de>
2024-06-06 13:55:57 +0200

    add fcitx

--- system/general.nix
+++ system/general.nix
@@ -40,18 +40,18 @@ in
     variant = "";
   };
 
-  #services.xserver.desktopManager.runXdgAutostartIfNone = true;
+  services.xserver.desktopManager.runXdgAutostartIfNone = true;
 
-  #i18n.inputMethod = {
-  #  enabled = "fcitx5";
-  #  fcitx5 = {
-  #    waylandFrontend = true;
-  #    addons = with pkgs; [
-  #      fcitx5-mozc
-  #      fcitx5-gtk
-  #    ];
-  #  };
-  #};
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+      ];
+    };
+  };
 
   powerManagement.enable = true;
   # Allow unfree packages
