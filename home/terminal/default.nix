81fb20d1e4248658b66c6c6e3c8ba279fed6fa63
Stefan Lahne <stefan.lahne@proton.me>
2024-10-27 05:54:11 +0100

    give git its own file

--- home/terminal/default.nix
+++ home/terminal/default.nix
@@ -8,27 +8,10 @@
     ./zsh.nix
     ./yazi.nix
     ./nixvim
+    ./git.nix
   ];
 
   programs = {
-    git = {
-      enable = true;
-      extraConfig = {
-        init.defaultBranch = "main";
-        # TODO: use fetchFromGitHub
-        include.path = "~/amnytas/home/terminal/themes.gitconfig";
-      };
-      # prettier lazygit, not 100% happy with this tho
-      delta = {
-        enable = true;
-        options = {
-          features = "villsau";
-          syntax-theme = "base16-stylix";
-          line-numbers = true;
-        };
-      };
-    };
-
     oh-my-posh = {
       enable = true;
       enableZshIntegration = true;
@@ -37,31 +20,6 @@
 
     zellij = {
       enable = true;
-    };
-  };
-
-  programs.lazygit = {
-    enable = true;
-    settings = {
-      # for some reason, without mkForce the colors all get weird
-      gui.theme = lib.mkForce {
-        activeBorderColor = ["magenta" "bold"];
-        inactiveBorderColor = ["cyan"];
-        searchingActiveBorderColor = ["cyan" "bold"];
-        optionsTextColor = ["cyan" "bold"];
-        selectedLineBgColor = ["default"];
-        inactiveViewSelectedBorderColor = ["bold"];
-        cherryPickedCommitFgColor = ["blue"];
-        cherryPickedCommitBgColor = ["cyan"];
-        markedBaseCommitFgColor = ["blue"];
-        markedBaseCommitBgColor = ["yellow"];
-        unstagedChangesColor = ["red"];
-        defaultFgColor = ["default"];
-      };
-      git.paging = {
-        colorArg = "always";
-        pager = "delta --paging=never";
-      };
     };
   };
 
