c3774f5cdabd677fa90e8d5394c47857a3d4d4e8
Stefan Lahne <stefan.lahne@proton.me>
2024-12-15 03:27:11 +0100

    re-enable v4l2loopback

--- system/default.nix
+++ system/default.nix
@@ -48,8 +48,13 @@
       tmp.cleanOnBoot = true;
       kernelModules = ["v4l2loopback"];
       supportedFilesystems = ["ntfs"];
-      extraModulePackages = with config.boot.kernelPackages; [v4l2loopback.out];
+      extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
+      extraModprobeConfig = ''
+        options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+      '';
     };
+
+    security.polkit.enable = true;
 
     networking.hostName = config.hostName;
 
