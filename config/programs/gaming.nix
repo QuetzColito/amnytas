6893b0de0d294d0091fab976e9aab28a295f29c9
Stefan Lahne <stefan.lahne@proton.me>
2025-04-07 14:17:15 +0200

    steam: fix cursor

--- config/programs/gaming.nix
+++ config/programs/gaming.nix
@@ -1,16 +1,19 @@
 {
   inputs,
   config,
+  theme,
   pkgs,
   ...
 }: {
   # HSR on Linux :>
   imports = [inputs.aagl.nixosModules.default];
   nix.settings = inputs.aagl.nixConfig; # Set up Cachix
+  # Dont try to add hsr before setting up cachix, unless you want to compile it
   programs.honkers-railway-launcher.enable = !config.firstInstall;
 
   programs.steam = {
     enable = true;
+    extraPackages = [theme.cursor.package]; # >.>
     protontricks.enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
