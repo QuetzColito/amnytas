b87cbe8f7b6e5b3e05d92017238960a35d377147
Stefan Lahne <stefan.lahne@proton.me>
2025-06-16 00:06:21 +0200

    peitha: reinstall

--- hosts/peitha.nix
+++ hosts/peitha.nix
@@ -2,31 +2,26 @@ _: {
   isNvidia = false;
   wantGrub = true;
 
-  system.stateVersion = "24.05"; # Did you read the comment?
-
-  fileSystems."/home/quetz/storage" = {
-    device = "/dev/sda4";
-    options = ["nofail"];
-  };
+  system.stateVersion = "25.05"; # Did you read the comment?
 
   autostartApps = true;
 
   monitors = [
     {
-      name = "DP-1";
+      name = "DP-4";
       coords = "0x0";
       workspaces = [4 5 6];
     }
     {
-      name = "HDMI-A-2";
+      name = "DP-5";
       coords = "2560x660";
-      workspaces = [7 8 9];
+      workspaces = [1 2 3 7 8 9];
     }
-    {
-      name = "DP-2";
-      coords = "-1080x50";
-      rotation = "1";
-      workspaces = [1 2 3];
-    }
+    # {
+    #   name = "DP-2";
+    #   coords = "-1080x50";
+    #   rotation = "1";
+    #   workspaces = [1 2 3];
+    # }
   ];
 }
