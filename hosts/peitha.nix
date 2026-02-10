ca93b96b9f2755bd507346b237f08ed484284fa5
Stefan Lahne <stefan.lahne@proton.me>
2026-02-10 09:11:36 +0100

    maybe fix peitha monitors

--- hosts/peitha.nix
+++ hosts/peitha.nix
@@ -31,5 +31,21 @@
       rotation = "1";
       workspaces = [1 2 3];
     }
+    {
+      name = "DP-1";
+      coords = "0x0";
+      workspaces = [4 5 6];
+    }
+    {
+      name = "HDMI-A-2";
+      coords = "2560x660";
+      workspaces = [7 8 9];
+    }
+    {
+      name = "DP-2";
+      coords = "-1080x50";
+      rotation = "1";
+      workspaces = [1 2 3];
+    }
   ];
 }
